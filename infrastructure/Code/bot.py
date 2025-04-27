31209b3fa4bcce1470c9af09e18476626d2f0b6d
triieu <144178085+triieu@users.noreply.github.com>
2025-04-27 11:22:57 -0500

    adjusting bot code to grab env

--- infrastructure/Code/bot.py
+++ infrastructure/Code/bot.py
@@ -1,15 +1,19 @@
 import os
 import discord
 import requests
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 from discord.ext import commands
 from discord import app_commands
 
-load_dotenv()
+# load_dotenv()
 
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 STEAM_API_KEY = os.getenv("STEAM_API_KEY")
 GUILD_ID = os.getenv("GUILD_ID")
+
+if not DISCORD_TOKEN or not STEAM_API_KEY or not GUILD_ID:
+    raise ValueError("env variables missing")
+
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix="!", intents=intents)
